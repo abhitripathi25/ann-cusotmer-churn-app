71aecc259a441ecf3498526dae7caeb79c0f5160
abhitripathi25 <142836144+abhitripathi25@users.noreply.github.com>
2026-06-24 23:38:51 +0530

    Modify churn prediction logic and output
    Updated the app to include a random prediction probability for customer churn.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder
-import tensorflow as tf
+
 import numpy as np
 import pickle
 import pandas as pd
-from tensorflow.keras.models import load_model
+
 
 
 
@@ -90,8 +90,10 @@ input_data_scaled=scaler_load.transform(input_data)
 # predict the churn
 
 
-prediction = model.predict(input_data_scaled)
-prediction_prob = prediction[0][0]
+import numpy as np
+
+prediction_prob = np.random.rand()
+
 
 # ✅ probability print karo
 st.write(f"Churn Probability: {prediction_prob:.4f}")
